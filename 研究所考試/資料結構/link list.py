960aeeeb013a23be19419204f8f89e5b72c83432
Leeyu <38371056+leeyu0911@users.noreply.github.com>
2020-03-16 17:55:19 +0800

    master

---
+++ 研究所考試/資料結構/link list.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def create_circular_linklist(n):
+    # 創建連續整數n個環狀串列
+    head = Node(1)
+    pre = head
+    for i in range(2, n+1):
+        new_node = Node(i)
+        pre.next = new_node
+        pre = new_node
+    pre.next = head
+    return head
+
+head = create_circular_linklist(3)
+print(head.next.value)
