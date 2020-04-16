e4b788ae6ded228e2db19106f807e1e2c22f726d
Leeyu <38371056+leeyu0911@users.noreply.github.com>
2020-04-16 21:12:35 +0800

    update

---
+++ leetcode/0678. Valid Parenthesis String.py
@@ -0,0 +1,73 @@
+'''
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+An empty string is also valid.
+Example 1:
+Input: "()"
+Output: True
+Example 2:
+Input: "(*)"
+Output: True
+Example 3:
+Input: "(*))"
+Output: True
+Note:
+The string size will be in the range [1, 100].
+'''
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack = []
+        star_stack = []
+        for i, ss in enumerate(s):
+            if ss == '(':
+                stack.append(i)  # 紀錄位置
+            elif ss == '*':
+                star_stack.append(i)  # 紀錄位置
+            else:
+                if ss == ')':
+                    if not stack:
+                        if not star_stack:
+                            return False
+                        else:
+                            star_stack.pop()
+                    else:
+                        stack.pop()
+        while stack and star_stack:
+            if stack.pop() > star_stack.pop():
+                return False
+        return True
+
+
+
+
+print(Solution.checkValidString(0, "(*))"))  # T
+print(Solution.checkValidString(0, "(())"))  # T
+print(Solution.checkValidString(0, "(*)("))  # F
+print(Solution.checkValidString(0, "(*)"))  # T
+print(Solution.checkValidString(0, ")"))  # F
+print(Solution.checkValidString(0, "(*()"))  # T
+print(Solution.checkValidString(0, "(((******))"))  # T
+print(Solution.checkValidString(0, ")"))  # F
+print(Solution.checkValidString(0, "((()))()(())(*()()())**(())()()()()((*()*))((*()*)"))  # T
+print(Solution.checkValidString(0, "(((((()*)(*)*))())())(()())())))((**)))))(()())()"))  # F
+print(Solution.checkValidString(0, "(())((())()()(*)(*()(())())())()()((()())((()))(*"))  # F
+print(Solution.checkValidString(0, "((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"))  # T
+
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        lo = hi = 0
+        for c in s:
+            lo += 1 if c == '(' else -1
+            hi += 1 if c != ')' else -1
+            if hi < 0:
+                break
+            lo = max(lo,0)
+
+        return lo == 0
---
+++ leetcode/30 days challenge/15. Product of Array Except Self.py
@@ -0,0 +1,70 @@
+'''
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+'''
+
+# https://leetcode.com/articles/product-of-array-except-self/
+
+class Solution:
+    def productExceptSelf(self, nums: 'List[int]') -> 'List[int]':
+        # O(n^2)
+        # n = len(nums)
+        # output_list = [None] * len(nums)
+        # for i in range(n):
+        #     product = 1
+        #     for j, num in enumerate(nums):
+        #         if i != j:
+        #             product *= num
+        #     output_list[i] = product
+        # return output_list
+
+        # O(n)
+        product = 1
+        n = len(nums)
+        output_list = []
+        for i in range(n):
+            output_list.append(product)
+            product = product * nums[i]
+        print(output_list)
+
+        product = 1
+        for i in reversed(range(n)):
+            output_list[i] = output_list[i] * product
+            product = product * nums[i]
+        return output_list
+
+
+print(Solution.productExceptSelf(0, [1,2,3,4]))
+print(Solution.productExceptSelf(0, [0, 0]))
+print(Solution.productExceptSelf(0, [1, 0]))
+
+print()
+class Solution:
+    def productExceptSelf(self, nums: 'list[int]') -> 'list[int]':
+        length = len(nums)
+        L, R, answer = [0] * length, [0] * length, [0] * length
+        L[0] = 1
+        for i in range(1, length):
+            L[i] = nums[i - 1] * L[i - 1]
+        print(L)
+
+        R[length - 1] = 1
+        for i in reversed(range(length - 1)):
+            R[i] = nums[i + 1] * R[i + 1]
+        print(R)
+
+        for i in range(length):
+            answer[i] = L[i] * R[i]
+        return answer
+
+print(Solution.productExceptSelf(0, [1, 2, 3, 4]))
