f63c69c2f383d5c222b6c72cedeae52394c8620c
Leeyu <38371056+leeyu0911@users.noreply.github.com>
2020-04-23 15:25:56 +0800

    Update 讀取資料夾檔案.py

--- 小程式/讀取資料夾檔案.py
+++ 小程式/讀取資料夾檔案.py
@@ -1,8 +1,32 @@
 import os
+import re
+import pandas as pd
 
-path = '/Users/leeyu/Downloads/新增包含項目的檔案夾/'
-file_list = os.listdir(path)
-# print(type(file_list))
+def main():
+    path = '/Users/leeyu/Downloads/新增包含項目的檔案夾/'
+    file_list = os.listdir(path)
+    # print(type(file_list))  # list
 
-for i in file_list:
-    print(i)
+    # Track_name = re.compile(r'')
+    # Artist_name = re.compile(r'')
+    # Playlist_name = ''
+
+    # csv_content = []
+    df = pd.DataFrame(file_list)
+
+    df.to_csv('list.csv')
+
+ 
+
+# test
+main()
+
+
+# for i in os.walk('/'):
+#     print(i)
+
+# for i in os.listdir(path):
+#     for j in os.listdir(path+'\\'+i):
+#         print(path+'\\'+i+'\\'+j)
+
+
