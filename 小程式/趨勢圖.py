09f552ac78eaa4266df9cfdd9c448f191f28b3a9
Yu <leeyu0911@gmail.com>
2020-08-04 20:53:22 +0800

    support windows

--- 小程式/趨勢圖.py
+++ 小程式/趨勢圖.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
+
 try:
     df = pd.read_excel('/Users/leeyu/OneDrive/NCU.xlsx')
-except:
+except FileNotFoundError:
     df = pd.read_excel(r'C:\Users\Yu\OneDrive\NCU.xlsx')
+
 # d109 = pd.DataFrame()
 # d109[0], d109[1] = df['d'][2:], df['i'][2:]
 # d108 = pd.DataFrame()
